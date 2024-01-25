7361b093888dd1ad8d9f6edafec5fe5a2b798c0b
MrAdisson <cedric.adisson@gmail.com>
2024-01-25 15:14:27 +0100

    more google friendly

--- main.js
+++ main.js
@@ -1,111 +1,152 @@
 import * as cocoSsd from '@tensorflow-models/coco-ssd';
 import '@tensorflow/tfjs';
-let isWebcamEnabled = false;
+
 let cameraFacingMode = 'environment';
-let firstStart = true;
-
-const webcamButton = document.getElementById('webcamButton');
+const video = document.getElementById('webcam');
 const liveView = document.getElementById('liveView');
-const webcam = document.getElementById('webcam');
-const ctx = document.getElementById('canvas').getContext('2d');
-let cameraWidth = 0;
-let cameraHeight = 0;
+const demosSection = document.getElementById('demos');
+const enableWebcamButton = document.getElementById('webcamButton');
 
-// not display the webcam
-webcam.style.display = 'none';
+/**
+ * @returns {boolean}
+ * @description Check if webcam access is supported in the browser.
+ * @see https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
+ */
+function getUserMediaSupported() {
+  return !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia);
+}
 
-const detectFromVideoFrame = (model) => {
-  // IF TEXTURE SIZE OxO return
-  if (webcam.videoWidth === 0 || webcam.videoHeight === 0) {
-    return;
-  }
-  const webcamElement = webcam;
-  const liveViewElement = liveView;
-  const videoWidth = cameraWidth;
-  const videoHeight = cameraHeight;
-  canvas.width = videoWidth;
-  canvas.height = videoHeight;
-  liveViewElement.width = videoWidth;
-  liveViewElement.height = videoHeight;
-  ctx.drawImage(webcamElement, 0, 0, videoWidth, videoHeight);
-  model.detect(webcamElement).then((predictions) => {
-    for (let n = 0; n < predictions.length; n++) {
-      const prediction = predictions[n];
-      ctx.beginPath();
-      ctx.lineWidth = '2';
-      ctx.strokeStyle = 'green';
-      ctx.fillStyle = 'black';
-      ctx.font = '18px Arial';
-      ctx.fillText(
-        prediction.class.charAt(0).toUpperCase() +
-          prediction.class.slice(1) +
-          ' ' +
-          Math.round(parseFloat(prediction.score) * 100) +
-          '% confidence',
-        prediction.bbox[0],
-        prediction.bbox[1] > 10 ? prediction.bbox[1] - 5 : 10
-      );
-      ctx.rect(prediction.bbox[0], prediction.bbox[1], prediction.bbox[2], prediction.bbox[3]);
-      ctx.fillStyle = '#10503020';
-      ctx.fill();
-      ctx.stroke();
-    }
+// If webcam supported, add event listener to button for when user
+if (getUserMediaSupported()) {
+  enableWebcamButton.addEventListener('click', enableCam);
+} else {
+  console.warn('getUserMedia() is not supported by your browser');
+}
 
-    requestAnimationFrame(() => detectFromVideoFrame(model));
-  });
-};
-const stopAICamera = () => {
-  webcam.srcObject.getVideoTracks().forEach((track) => track.stop());
-  webcam.removeEventListener('loadeddata', () => {
-    console.log('loadeddata');
-  });
-};
+// Enable the live webcam view and start classification.
+function enableCam(event) {
+  // Only continue if the COCO-SSD has finished loading.
+  if (!model) return;
 
-const startAICamera = async (model, facingMode) => {
-  const stream = await navigator.mediaDevices.getUserMedia({
+  // Hide the button once clicked.
+  event.target.disabled = true;
+
+  // getUsermedia parameters to force video but not audio.
+  const constraints = {
     video: {
-      facingMode: facingMode,
+      facingMode: cameraFacingMode,
     },
-    audio: false,
-  });
-  webcam.srcObject = stream;
-  if (!firstStart) return;
-  firstStart = false;
-  webcam.addEventListener('loadeddata', () => {
-    cameraWidth = webcam.videoWidth;
-    cameraHeight = webcam.videoHeight;
-    console.log('cameraWidth', cameraWidth);
-    console.log('cameraHeight', cameraHeight);
-    canvas.width = cameraWidth;
-    canvas.height = cameraHeight;
-    detectFromVideoFrame(model);
-  });
+  };
 
-  canvas.addEventListener('click', () => {
-    console.log('CLICK');
-    stopAICamera();
-    if (cameraFacingMode === 'environment') {
-      cameraFacingMode = 'user';
-    } else {
-      cameraFacingMode = 'environment';
-    }
-    startAICamera(model, cameraFacingMode);
+  // Activate the webcam stream.
+  navigator.mediaDevices.getUserMedia(constraints).then(function (stream) {
+    video.srcObject = stream;
+    video.addEventListener('loadeddata', predictWebcam);
   });
-};
+}
+
+let model = null;
 
 cocoSsd.load().then((loadedModel) => {
   console.log('Model loaded: ', loadedModel);
-  // enable webcam button
-  webcamButton.disabled = false;
-  webcamButton.innerText = 'Enable webcam';
-
-  webcamButton.addEventListener('click', async () => {
-    if (isWebcamEnabled) return;
-    isWebcamEnabled = true;
-    console.log('Button clicked');
-    console.log(navigator.mediaDevices);
-    startAICamera(loadedModel, cameraFacingMode);
-  });
+  model = loadedModel;
+  demosSection.classList.remove('invisible');
+  enableWebcamButton.disabled = false;
+  enableWebcamButton.innerText = 'Enable webcam';
 });
 
-// ON CLICK ON VIDEO, TURN CAMERA (ENVIRONMENT OR USER)
+let children = [];
+
+function predictWebcam() {
+  // Now let's start classifying a frame in the stream.
+  model.detect(video).then(function (predictions) {
+    // Remove any highlighting we did previous frame.
+    for (let i = 0; i < children.length; i++) {
+      liveView.removeChild(children[i]);
+    }
+    children.splice(0);
+
+    // Now lets loop through predictions and draw them to the live view if
+    // they have a high confidence score.
+    for (let n = 0; n < predictions.length; n++) {
+      // If we are over 66% sure we are sure we classified it right, draw it!
+      if (predictions[n].score > 0.66) {
+        const p = document.createElement('p');
+        p.innerText =
+          predictions[n].class + ' - with ' + Math.round(parseFloat(predictions[n].score) * 100) + '% confidence.';
+        p.style =
+          'margin-left: ' +
+          predictions[n].bbox[0] +
+          'px; margin-top: ' +
+          predictions[n].bbox[1] +
+          'px; width: ' +
+          predictions[n].bbox[2] +
+          'px; top: 0; left: 0;';
+        const highlighter = document.createElement('div');
+        highlighter.setAttribute('class', 'highlighter');
+        highlighter.style =
+          'left: ' +
+          predictions[n].bbox[0] +
+          'px; top: ' +
+          predictions[n].bbox[1] +
+          'px; width: ' +
+          predictions[n].bbox[2] +
+          'px; height: ' +
+          predictions[n].bbox[3] +
+          'px;';
+
+        liveView.appendChild(highlighter);
+        liveView.appendChild(p);
+        children.push(highlighter);
+        children.push(p);
+      }
+    }
+
+    // Call this function again to keep predicting when the browser is ready.
+    window.requestAnimationFrame(predictWebcam);
+  });
+}
+
+// const detectFromVideoFrame = (model) => {
+//   model.detect(video).then((predictions) => {
+//     for (let n = 0; n < predictions.length; n++) {
+//       const prediction = predictions[n];
+//     }
+
+//     requestAnimationFrame(() => detectFromVideoFrame(model));
+//   });
+// };
+// const stopAICamera = () => {
+//   webcam.srcObject.getVideoTracks().forEach((track) => track.stop());
+//   webcam.removeEventListener('loadeddata', () => {
+//     console.log('loadeddata');
+//   });
+// };
+
+// const startAICamera = async (model, facingMode) => {
+//   const stream = await navigator.mediaDevices.getUserMedia({
+//     video: {
+//       facingMode: facingMode,
+//     },
+//     audio: false,
+//   });
+//   webcam.srcObject = stream;
+//   if (!firstStart) return;
+//   firstStart = false;
+//   webcam.addEventListener('loadeddata', () => {
+//     // cameraWidth = webcam.videoWidth;
+//     // cameraHeight = webcam.videoHeight;
+//     detectFromVideoFrame(model);
+//   });
+
+//   // canvas.addEventListener('click', () => {
+//   //   console.log('CLICK');
+//   //   stopAICamera();
+//   //   if (cameraFacingMode === 'environment') {
+//   //     cameraFacingMode = 'user';
+//   //   } else {
+//   //     cameraFacingMode = 'environment';
+//   //   }
+//   //   startAICamera(model, cameraFacingMode);
+//   // });
+// };
